c03431757f69680314e41125219a3effb77de6f7
Ryan Cao <hello@ryanccn.dev>
2026-05-23 22:54:50 +0100

    fix: update outdated Nerd Fonts hash

--- _manifest.nix
+++ _manifest.nix
@@ -9,6 +9,6 @@
 
   nerd-fonts = {
     version = "3.4.0";
-    hash = "sha256-JR4sxV2yOXtrnIjFBh4as304BjNIcKkBzxOKLxrjo2I=";
+    hash = "sha256-koZj0Tn1HtvvSbQGTc3RbXQdUU4qJwgClOVq1RXW6aM=";
   };
 }
